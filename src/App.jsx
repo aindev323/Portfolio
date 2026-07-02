a664055c2cbf83773295310de0e0bcddb0baeeee
Ain <aindev323@gmail.com>
2026-07-02 10:50:12 +0900

    update skip-nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Skills from "./components/Skills/Skills";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import './App.css';
 
 function App() {
   return (
     <>
+      <a href="#main-content" className="skip-nav">본문 바로가기</a>
       <Nav />
-      <main>
+      <main id="main-content" tabIndex="-1">
         <Hero />
         <About />
         <Skills />
